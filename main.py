b3d26f7ef79550a332ab4be09d61caf93938fb09
Rad <145648587+not-dika@users.noreply.github.com>
2024-03-05 09:09:28 +0000

    yes

---
+++ main.py
@@ -0,0 +1,7 @@
+scanner = input("Apakah anda memiliki kartu perpustakaan (ya/tidak) \n")
+if scanner == "ya":
+    print("access granted")
+elif scanner == "tidak":
+    print("access denied ")
+else:
+    print("pilihan tidak valid")
